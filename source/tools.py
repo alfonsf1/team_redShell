ae2df5ee7d7576a7f549fed55cd91e4b3d19e4a9
mirandofrank <mirandofrank@csu.fullerton.edu>
2019-10-27 17:26:34 -0700

    finished setup and tools

---
+++ source/tools.py
@@ -0,0 +1,11 @@
+import os
+import pygame as pg
+from abc import ABC, abstractmethod
+
+keybinding = {
+    'action':pg.K_s,
+    'jump':pg.K_a,
+    'left':pg.K_LEFT,
+    'right':pg.K_RIGHT,
+    'down':pg.K_DOWN
+}
